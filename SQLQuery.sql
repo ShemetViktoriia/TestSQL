4fa5783c90a7315f3faa2484b4834841edfc4694
ShemetViktoriya <shemetvic@ukr.net>
2018-05-18 02:07:17 +0300

    fix

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -5,18 +5,6 @@ GO
 use TEST_EMPLOYEES;
 GO
 
-/*
-—оздать таблицу EMPLOYEES
-с пол€ми
-ID 
-F_NAME (»м€)
-L_NAME (‘амили€)
-FIX_PAYMENT (T - фиксированна€ оплата,  F - почасова€ оплата)
-COUNT_DAYS (кол-во отработанных дней за мес€ц)
-COUNT_HOUR (кол-во отработанных часов за мес€ц) 
-RATE (сумма ставки если FIX_PAYMENT = 'T' то за мес€ц иначе за час)
-*/
-
 CREATE TABLE Employees 
 (
 	ID INT PRIMARY KEY IDENTITY,
@@ -30,12 +18,6 @@ CREATE TABLE Employees
 );
 GO
 
-/*
-—оздать метод дл€ наполнени€ таблицы данными.
-¬вод по одному сотруднику.
-≈сли тип оплаты почасовой необходимо ввести кол-во отработанных часов
-—тавка об€зательна
-*/
 --STORED PROCEDURE AddOneEmployee
 CREATE PROC sp_AddEmployee
 	@f_name NVARCHAR(20),
@@ -56,57 +38,47 @@ AS
 GO	
 
 -- ¬вод по одному сотруднику
--- 1-й сотрудник
+-- 1 Employee
 EXEC sp_AddEmployee
 	 @f_name ='Ivan',
 	 @l_name = 'Ivanov',
 	 @count_days = 20, 
 	 @rate = 8000
--- 2-й сотрудник
+-- 2 Employee
 EXEC sp_AddEmployee 'Anton', 'Sidorov', 'F', 15, 172, 53.5
--- 3-й сотрудник
+-- 3 Employee
 EXEC sp_AddEmployee 'Stepan', 'Gavrilov',  'T', 25, 0, 10500
--- 4-й сотрудник
+-- 4 Employee
 EXEC sp_AddEmployee 'Anna', 'Antonova',  'T', 20, 0, 9000
--- 5-й сотрудник
+-- 5 Employee
 EXEC sp_AddEmployee 'Olga', 'Fedorova',  'F', 19, 188, 49.6
 GO
 
--- ќтображение таблицы сотрудников
 EXEC sp_GetAllEmployees
 GO
-
-/*
-—оздать метод расчет заработной платы за мес€ц по сотруднику 
-ѕринимает значение ‘амили€ и »м€
-ѕоказывает сумму з/п
-≈сли сотрудник FIX_PAYMENT = 'T' вычислить кол-во рабочих дней в мес€це без субб и воскр
-(праздники не учитываем) можно вз€ть мес€ц февраль
-если FIX_PAYMENT = 'F' просто умножаем кол-во часов на ставку
-*/
 
 CREATE PROCEDURE sp_CalculateSalary
 	@f_name NVARCHAR(15),
 	@l_name NVARCHAR(15)
 AS
-	--–асчет кол-ва дней в мес€це без суббот и воскресений за период дат
+	-- Count days between dates
 	DECLARE @work_days TINYINT, @start SMALLDATETIME, @end SMALLDATETIME
 	SELECT @start = '20180201', @end = '20180228'
 	SET @work_days =(SELECT DAY(DATEADD(Month, 1, @start) - DAY(DATEADD(Month, 1, @start))) -- всего дней в промежутке указанных дат
-			-  datediff (wk, @start - 7, @end - 6) -- кол-во суббот в промежутке дат
-				- datediff (wk, @start - 8, @end - 7) )-- кол-во воскресений в промежутке дат
-    -- Ћогика вывода суммы заработной платы сотрудника в зависимости от формы оплаты
+			-  datediff (wk, @start - 7, @end - 6) --  count saturdays between dates
+				- datediff (wk, @start - 8, @end - 7) )-- count sundays between dates
+   
 	DECLARE @fix_payment VARCHAR(1), @rate DECIMAL(8,2), @count_hour TINYINT
 	SELECT @fix_payment = FIX_PAYMENT, @rate = RATE, @count_hour =COUNT_HOUR
     FROM Employees
     WHERE (F_NAME = @f_name AND L_NAME = @l_name)
 IF @fix_payment = 'T'
-	PRINT @l_name + ' ' + @f_name + ' - ' + CONVERT(NVARCHAR, @rate)  + ' грн. ставки, в феврале за ' + CONVERT(NVARCHAR, @work_days) + ' рабочих дней'
+	PRINT @l_name + ' ' + @f_name + ' - ' + CONVERT(NVARCHAR, @rate)  + ' UAH, in february?  ' + CONVERT(NVARCHAR, @work_days) + ' works days'
 ELSE 
-	PRINT @l_name + ' ' + @f_name + ' - ' + CONVERT(NVARCHAR, @rate*@count_hour) + ' грн. заработка за мес€ц'
+	PRINT @l_name + ' ' + @f_name + ' - ' + CONVERT(NVARCHAR, @rate*@count_hour) + ' UAH in february'
 GO
 
--- –асчет заработной платы за мес€ц по всем сотрудникам
+-- Calculation salary for the month for all employees
 EXEC sp_CalculateSalary 'Ivan', 'Ivanov'
 EXEC sp_CalculateSalary 'Anton', 'Sidorov'
 EXEC sp_CalculateSalary 'Stepan', 'Gavrilov'
